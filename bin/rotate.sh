02e985c73362cfb110f90602db4ec7683acc6a53
quant <quant@happy>
2023-07-23 19:32:22 +0800

    update rotate.sh can random select pictures from wallpaper directory as different background.
    Changes to be committed:
          modified:   rotate.sh
          new file:   wallpaper.sh
          modified:   ../config
          new file:   ../wallpaper/h/pexels-arshad-sutar-1749303.jpg
          new file:   ../wallpaper/h/pexels-asad-photo-maldives-1450360.jpg
          new file:   ../wallpaper/h/pexels-dustin-cox-159884.jpg
          new file:   ../wallpaper/h/pexels-englishname-2734421.jpg
          new file:   ../wallpaper/h/pexels-james-wheeler-417074.jpg
          new file:   ../wallpaper/h/pexels-johannes-plenio-1146708.jpg
          new file:   ../wallpaper/h/pexels-nextvoyage-490411.jpg
          new file:   ../wallpaper/h/pexels-pixabay-163872.jpg
          new file:   ../wallpaper/h/pexels-pixabay-301395.jpg
          new file:   ../wallpaper/h/pexels-pixabay-33545.jpg
          new file:   ../wallpaper/h/pexels-pixabay-371589.jpg
          new file:   ../wallpaper/h/pexels-pixabay-63324.jpg
          new file:   ../wallpaper/h/pexels-vlad-kovriga-339119.jpg
          new file:   ../wallpaper/h/pexels-zukiman-mohamad-21787.jpg
          new file:   ../wallpaper/v/pexels-artem-saranin-1496373.jpg
          new file:   ../wallpaper/v/pexels-danila-giancipoli-2242171.jpg
          new file:   ../wallpaper/v/pexels-francesco-ungaro-1526713.jpg
          new file:   ../wallpaper/v/pexels-guillaume-meurice-1544376.jpg

--- bin/rotate.sh
+++ bin/rotate.sh
@@ -1,12 +1,53 @@
 #!/usr/bin/env bash
 
+# Initialize the RANDOM variable
+RANDOM=$$
+# Function to randomly select a picture file from a given directory
+# and return the file full path
+
+randomFile(){
+
+  # dir_path="${HOME}/.config/i3/wallpaper/h"
+  
+  # Check if the directory path exists
+  if [ ! -d "$1" ]; then
+     #return ""
+     echo "dir_pir_path is empty"
+
+  fi
+
+  # Get a list of all picture files in the directory
+  picture_files=$(find $1 -type f -name "*.jpg" -o -name "*.png")
+  # Split the string by space
+  picture_files=(${picture_files// / })
+  file_numbers=${#picture_files[@]}
+  # echo "$file_numbers"
+  random=$((${RANDOM} % ${file_numbers}))
+  random_file=${picture_files[random]}
+
+  echo $random_file
+}
+  
+h_path="$HOME/.config/i3/wallpaper/h"
+h_path_1=$(randomFile $h_path)
+h_path_2=$(randomFile $h_path)
+echo "horizontal pic1: ${h_path_1}"
+echo "horizontal pic2: ${h_path_2}"
+
+v_path="${HOME}/.config/i3/wallpaper/v"
+v_path=$(randomFile $v_path)
+echo "vertical pic: ${v_path}"
+
 output=$(/usr/bin/xrandr |grep DP-0|grep 2560x1440)
-echo $output
 if [ -z "$output" ]
 then
-  echo "Rotated"
-  exec /usr/bin/xrandr --output DP-0 --rotate normal
+  echo "Rotated convert to Normal"
+  eval "/usr/bin/xrandr --output DP-0 --rotate normal"
+  eval "/usr/bin/feh --bg-scale ${h_path_1} --bg-scale ${h_path_2}"
 else
-  echo "Not Rotated"
-  exec /usr/bin/xrandr --output DP-0 --rotate right
+  echo "Normal convert to Rotated"
+  eval "/usr/bin/xrandr --output DP-0 --rotate right"
+  eval "/usr/bin/feh --bg-scale ${v_path}  --bg-scale ${h_path_1}"
 fi
+
+exit 0
---
+++ bin/wallpaper.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+exec feh --bg-fill ~/.downloads/pexels-danila-giancipoli-2242171.jpg  --bg-fill ~/.downloads/pexels-pixabay-301395.jpg
