b9d4670fcd60267e5e3ae29a1c451ab5fc0bcca7
git <git@git>
2023-12-30 21:03:30 +0800

    update

--- bin/bluetooth.sh
+++ bin/bluetooth.sh
@@ -11,6 +11,6 @@ if [ -z "$pid" ]; then
   kill -9 $pid
 fi
 
-/usr/bin/pulseaudio --start
-sudo /etc/init.d/alsasound restart
-/usr/bin/bluetoothctl connect 04:52:C7:D1:BA:2C
+#/usr/bin/pulseaudio --start
+#sudo /etc/init.d/alsasound restart
+#/usr/bin/bluetoothctl connect 04:52:C7:D1:BA:2C
--- bin/nnn.sh
+++ bin/nnn.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 #nnn
-export NNN_PLUG='f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:imgview;i:cdonquit;'
+export NNN_PLUG='l:launch;j:autojump;f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:imgview;i:cdonquit;'
 export VISUAL="nvim"
 export NNN_TRASH=1
+
+
+source ~/.bashrc
 
 # print ;i to exec cdonquit
 nnn_cd()                                                                                                   
